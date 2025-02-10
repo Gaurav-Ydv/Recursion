d087bb4b5aa06d1c5bd74f80b3570331fa09b43c
Gaurav Yadav <156690582+Gaurav-Ydv@users.noreply.github.com>
2025-02-10 11:12:04 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static void printDecreasing(int n){  // 5 4 3 2 1
+        //base case
+        if(n == 1){
+            System.out.println(1);
+            return;
+        }
+        //self work
+        System.out.println(n); // this line print n
+        //recursive work
+        printDecreasing(n-1);  // n-1, n-2, n-3,......1
+    }
+
+    static void printIncreasing(int n){  //1, 2, 3,.....,n-1, n
+        //base case
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+        //recursive work
+        printIncreasing(n-1);  //1, 2,.....n-1
+        //Self work
+        System.out.println(n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printIncreasing(n);
+        printDecreasing(n);
+    }
+}
