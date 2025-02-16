e488c6c8b7ab7d07c17676dd9533e5013ff61743
Gaurav Kumar Yadav <156690582+Gaurav-Ydv@users.noreply.github.com>
2025-02-16 16:15:47 +0530

    Update powerOfNumber.java

--- powerOfNumber.java
+++ powerOfNumber.java
@@ -3,7 +3,7 @@ package Recursion;
 import java.util.Scanner;
 
 public class powerOfNumber {
-////--------------Approach-1 to find the power of a number in which TIME COMPLEXITY and SPACE COMPLEXITY = O(q)-----------------
+////--------------Approach-1 to find the power of a number where------TIME COMPLEXITY and SPACE COMPLEXITY = O(q)-----------------
 //    static int pow(int p, int q){  //p^q -> TC - O(q), where q is the power of p
 //        //base case
 //        if(q == 0){
@@ -13,8 +13,8 @@ public class powerOfNumber {
 //        return pow(p,q-1) * p;  // p^q-1 * p = p^q
 //    }
 
-//----------------------------------------------BETTER APPROACH---------------------------------------------------
-//----------------Approach-2 to find the power of a number in which TIME COMPLEXITY and SPACE COMPLEXITY = O(log q)--------------
+//---------------------------------------------------BETTER APPROACH-------------------------------------------------------------
+//----------------Approach-2 to find the power of a number where -----TIME COMPLEXITY and SPACE COMPLEXITY = O(log q)--------------
     static int pow(int p, int q){
         //base case
         if(q == 0) return 1;
