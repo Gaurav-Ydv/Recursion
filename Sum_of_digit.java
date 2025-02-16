67f344b1151941b78567254debeda307bde72839
Gaurav Yadav <156690582+Gaurav-Ydv@users.noreply.github.com>
2025-02-16 11:24:01 +0530

    Update Sum_of_digit.java

--- Sum_of_digit.java
+++ Sum_of_digit.java
@@ -3,26 +3,19 @@ package Recursion;
 import java.util.Scanner;
 
 public class Sum_of_digit {
-
     static int sumOfDigits(int n){
         //base case
         if(n >= 0 && n <= 9) {
             return n;
         }
 
-//------simplest form of below recursive work and self work
+//------shortest form of below recursive work and self work
         return sumOfDigits(n/10) + n % 10;
+        
 //        //recursive work -> small ans
 //        int smallAns =  sumOfDigits(n/10);
-//
 //        //self work
 //        return smallAns + n%10;
-    }
-    static int count(int n){
-        int count = 0;
-        if(n >= 0)
-            count++;
-        return count;
     }
 
     public static void main(String[] args) {
@@ -33,10 +26,5 @@ public class Sum_of_digit {
         int m = sc.nextInt();
         System.out.println("Sum of the digit is : ");
         System.out.println(sumOfDigits(m));
-        System.out.println("total digit is : ");
-        System.out.println(count(m));
-
-
-
     }
 }
